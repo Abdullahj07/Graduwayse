dd33a3423c4dedb4937f7240b13c23853e6fec26
Abdullah Jeway <abdullahjeway7@gmail.com>
2026-05-05 14:59:22 +0100

    Openshift allow hosts to deploy

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", os.getenv("SECRET_KEY", "dev-only-secret-key"))
 DEBUG = os.getenv("DEBUG", "True") == "True"
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "daphne",
